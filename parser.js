4f79a5c169fc20d6c4e23a192f7e061bb8e235d3
JakubIwanowski <49kU3dQzDPdO>
2019-09-17 17:00:40 +0200

    finished

--- parser.js
+++ parser.js
@@ -12,20 +12,32 @@ function getHeaders(arr) {
     let headers = [arr[0]]
     headers = headers[0].split(',')
     return headers;
+   
 }
 const headings = getHeaders(csvArray)
 csvArray.shift();
 let json = [];
+let counter = 0
 const jsonObject = csvArray.forEach((el) => {
-    let counter = 0
-    let splited = el.split(',')
-    let arr = for (let i = 0; i < headings.length; i++) {
-      
-        }
-    obj[counter] = {
 
-    }
+    let splited = el.split(',')
+    json[counter] = {}
+for (let i = 0; i < headings.length; i ++) {
+
+    json[counter][headings[i]] = splited[i]
+}
+
+   counter++
+ 
+    
+   
 })
+json = JSON.stringify(json).replace(/\\r/g, "")
+
+        fs.writeFile('customer-data.json', json, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
 });
 
 
